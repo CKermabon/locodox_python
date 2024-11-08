5a05741bcaf76154ad4980a5dc1dcbb8b22a30c9
C. Kermabon <catherine.kermabon@ifremer.fr>
2024-11-08 11:37:05 +0100

    Ajout m_argo_data contenant lecture Rtraj/Sprof

---
+++ m_argo_data.py
@@ -0,0 +1,38 @@
+""" Module dedie a la lecture des donnees ARGO """
+# Importation des modules necessaires
+import argopy
+import xarray as xr
+import glob
+import numpy as np
+
+# Fonction de lecture
+def read_argo_data(num_float,rep_data_argo):
+	""" 
+	Cette fonction lit les fichiers Rtraj et Sprof d'un flotteur.
+	En entree : 
+ 		num_float : numero WMO du flotteur
+ 		rep_data_argo : repertoire ou sont les donnees ARGO
+	En sortie :
+ 		ds_argo_Rtraj_inair : donnees Rtraj contenant les donnees PPOX_DOXY dans l'air
+ 		ds_argo_Rtraj_inwater : donnees Rtraj contenant les donnees PPOX_DOXY proche de la surface
+ 		ds_argo_Sprof : donnees synthetique
+	"""
+	fic_argo_Sprof = glob.glob(rep_data_argo + num_float + '/*Sprof.nc')
+	fic_argo_Rtraj = glob.glob(rep_data_argo + num_float + '/*Rtraj.nc')
+	ds_argo_Rtraj = xr.open_dataset(fic_argo_Rtraj[0],engine='argo')
+	ds_argo_Sprof = xr.open_dataset(fic_argo_Sprof[0],engine='argo')
+
+	# Dans le fichier Rtraj, il n'y a pas de longitude affectee aux donnees PPOX_DOXY dans l'air.
+	# On remplit les donnees de LONGITUDE à partir des donnees LONGITUDE des donnees synthetiques
+	# en faisant le lien entre les donnees Rtraj et Sprof via le numero de cycle.
+	ds_argo_Rtraj =  ds_argo_Rtraj.groupby("CYCLE_NUMBER").apply(
+    	 lambda group: group.assign_coords(
+        	LONGITUDE=("N_MEASUREMENT",np.full(len(group["N_MEASUREMENT"]),ds_argo_Sprof.LONGITUDE.sel(N_PROF=group.CYCLE_NUMBER[0]).item()))
+         )
+	)
+	# On ne garde que les donnees Rtraj qui nous interessent, à savoir les donnees dans l'air et proche de la surface. 
+	ds_argo_Rtraj_inair = ds_argo_Rtraj.where(ds_argo_Rtraj['MEASUREMENT_CODE'].isin([699,711,799]),drop=True)
+	ds_argo_Rtraj_inwater = ds_argo_Rtraj.where(ds_argo_Rtraj['MEASUREMENT_CODE'].isin([690,710]),drop=True)
+
+	return ds_argo_Rtraj_inair, ds_argo_Rtraj_inwater, ds_argo_Sprof
+
---
+++ test.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+from m_argo_data import read_argo_data
+
+if __name__ == "__main__":
+	rep_argo_data = '/Users/chemon/ARGO_NEW/NEW_LOCODOX/data_test/'
+	num_float = '6902802'
+	ds_argo_inair, ds_argo_inwater,ds_argo_Sprof = read_argo_data(num_float,rep_argo_data)
+#	print(ds_argo_inair)
+#	print(ds_argo_inwater)
+#	plt.figure()
+#	plt.subplot(2,2,1)
+#	plt.plot(ds_argo_inair['LONGITUDE'])
+#	plt.subplot(2,2,2)
+#	plt.plot(ds_argo_inair['PPOX_DOXY'])
+#	plt.subplot(2,2,3)
+#	plt.plot(ds_argo_inwater['LONGITUDE'])
+#	plt.subplot(2,2,4)
+#	plt.plot(ds_argo_inwater['PPOX_DOXY'])
+#	plt.show()
+	plt.figure()
+	plt.plot(ds_argo_inair['CYCLE_NUMBER'],ds_argo_inair['LONGITUDE'],'+b')
+	plt.plot(ds_argo_Sprof['CYCLE_NUMBER'],ds_argo_Sprof['LONGITUDE'],'xr') 
+	plt.show()
