24e36900d966344ef7beb241286f1ead9c6a67df
C. Kermabon <catherine.kermabon@ifremer.fr>
2024-11-29 15:49:37 +0100

    m_WOA_data.py : Extrapolation des donnees WOA aux jours <15 et > 350 afin de pouvoir interpoler les donnees WOA sur toute l'annee (Dans le fichier WOA, time va de 15 à 350 par pas de 30)

--- m_WOA_data.py
+++ m_WOA_data.py
@@ -18,10 +18,38 @@ def read_WOA(fic_WOA,lon_argo,lat_argo,time_argo):
     # Passage des longitudes de [0 360] a [-180 180]
     #
     ds_woa['lon'] = xr.where(ds_woa['lon'] > 180, ds_woa['lon'] - 360, ds_woa['lon'])
+
+    #
+    # Dans WOA, les donnees sont affectees a un jour dans l'annee.
+    # Les jours vont de 15 à 350. Pour interpoler sur les jours de l'annee (de 1 a 365),
+    # on extrapole les donnees WOA.
+    #
+
+    var_to_extend = [var for var in ds_woa.data_vars if "time" in ds_woa[var].dims]
+    new_time = xr.concat([
+        ds_woa['time'][-1] - 365.25, 
+        ds_woa['time'],                
+        ds_woa['time'][0] + 365.25 # Après le dernier
+    ], dim='time')
+    
+    extended_var = {}
+    for var in var_to_extend:
+        print(f'Extrapolation WOA temporelle en cours pour la variable : {var}')
+        data_en_cours = ds_woa[var]
+        first_data = data_en_cours.isel(time=0)
+        last_data = data_en_cours.isel(time=-1)
+        extended_data = xr.concat([first_data,data_en_cours,last_data],dim='time')
+        extended_data = extended_data.assign_coords(time=new_time)
+        extended_var[var] = extended_data 
+
+    ds_woa_extended = ds_woa.copy()   
+    for var, data in extended_var.items():
+        ds_woa_extended[var] = data.assign_coords(time=new_time)
+
     #
     # Interpolation des donnees WOA sur lon/lat/time ARGO.
     #
-    ds_woa_interp = ds_woa.interp(lat=lat_argo,lon=lon_argo,time=time_argo.dt.dayofyear)
-
+    ds_woa_interp = ds_woa_extended.interp(lat=lat_argo,lon=lon_argo,time=time_argo.dt.dayofyear)
+    ds_woa_interp = ds_woa_interp.transpose() # Les tableaux du dataset ont la dimension (N_CYCLE,N_DEPTH)
 
     return ds_woa_interp
